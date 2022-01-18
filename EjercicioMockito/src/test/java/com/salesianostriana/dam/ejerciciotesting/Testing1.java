ee2b03b3d176674f917d0efbd9e684cc3e7b82a4
Alfonso Gallardo Rodríguez <gallardo.roalf20@triana.salesianos.edu>
2022-01-18 14:26:50 +0100

    Añadiendo ejercicio testing

---
+++ EjercicioMockito/src/test/java/com/salesianostriana/dam/ejerciciotesting/Testing1.java
@@ -0,0 +1,19 @@
+package com.salesianostriana.dam.ejerciciotesting;
+
+import com.salesianostriana.dam.ejerciciotesting.repos.Repositorio;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.security.Provider;
+
+@ExtendWith(MockitoExtension.class)
+public class Testing1 {
+
+    @Mock
+    Repositorio repositorio;
+
+
+
+}
